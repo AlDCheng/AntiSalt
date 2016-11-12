ae71ff9d5e0178383d73e8e2bc27fce5ce491cdd
aldcheng <adcheng@mit.edu>
2016-11-11 23:56:09 -0500

    Basic Haar Cascade Face Detection

--- face_detection/face_detect_main.py
+++ face_detection/face_detect_main.py
@@ -1,7 +1,13 @@
 import numpy as np
 import cv2
+import cv2.cv as cv
 
 cap = cv2.VideoCapture(0)
+cap.set(cv.CV_CAP_PROP_FRAME_WIDTH, 480)
+cap.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 320)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#image = np.zeros((480,640,3), np.uint8)
 
 while(True):
     # Capture frame-by-frame
@@ -10,8 +16,14 @@ while(True):
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    #print "Found "+str(len(faces))+" face(s)"
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
+
     # Display the resulting frame
     cv2.imshow('frame',gray)
+    cv2.imshow('faces',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
